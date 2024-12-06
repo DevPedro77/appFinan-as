eed9e860e5e36b07f34c60d5f133fd8b2e747a8a
Pedro Castro <pcastro9876@gmail.com>
2024-12-06 10:36:28 -0300

    criando o componente pra registrar

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import { Background, Input, SubmitButton, SubmitText } from './style';
 import { SafeAreaView, TouchableWithoutFeedback, Keyboard } from 'react-native';
@@ -6,6 +6,9 @@ import { SafeAreaView, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import Header from '../../components/Header'
 
 export default function RegisterForm(){
+  const [labelInput, setLabelInput] =useState('');
+  const [valueInput, setValueInput] =useState('');
+  const [type, setType] =useState('Receita');
   return(
     <TouchableWithoutFeedback onPress={ () => Keyboard.dismiss() } >
       <Background>
@@ -14,11 +17,15 @@ export default function RegisterForm(){
         <SafeAreaView style={{marginTop: 14, alignItems: 'center' }}>
           <Input
             placeholder="Descrição desse registro"
+            value={labelInput}
+            onChangeText={ (text) =>setLabelInput(text)}
           />
 
           <Input
             placeholder="Valor desejado"
             keyboardType="numeric"
+            value={valueInput}
+            onChangeText={ (text) => setValueInput(text)}
           />
 
           <SubmitButton>

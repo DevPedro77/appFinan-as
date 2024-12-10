cc4c7a7a16364ab83a5463a4f886be92735f527c
Pedro Castro <pcastro9876@gmail.com>
2024-12-10 15:09:28 -0300

    registrar gastos no banco

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,15 +1,59 @@
 import React, {useState} from 'react';
 
 import { Background, Input, SubmitButton, SubmitText } from './style';
-import { SafeAreaView, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import { SafeAreaView, TouchableWithoutFeedback, Keyboard, Alert } from 'react-native';
 
 import Header from '../../components/Header'
 import RegisterType from '../../components/RegisterType';
+import api from '../../services/api';
+import {format} from 'date-fns';
+import {useNavigation} from '@react-navigation/native'
 
 export default function RegisterForm(){
   const [labelInput, setLabelInput] =useState('');
   const [valueInput, setValueInput] =useState('');
   const [type, setType] =useState('receita');
+
+  const navigation = useNavigation()
+
+  function handleSubmit(){
+    Keyboard.dismiss()
+
+    if(isNaN(parseFloat(valueInput)) || type === null){
+      alert('Preencha todos os campos')
+      return;
+    }
+
+    Alert.alert(
+      'Confirmando todos dados', 
+      `Tipo ${type} - Valor: ${parseFloat(valueInput)}`,
+      [
+        {
+          text: 'Cancelar',
+          style: 'cancel'
+        },
+        {
+          text: 'Continuar',
+          onPress: () =>handleAdd()
+        }
+      ]
+    )
+  }
+
+  async function handleAdd() {
+    Keyboard.dismiss()
+
+    await api.post('/receive',{
+      description: labelInput,
+      value: Number(valueInput),
+      type: type,
+      date: format(new Date(), 'dd/MM/yyyy')
+    })
+
+    setLabelInput('')
+    setValueInput('')
+    navigation.navigate('Home')
+  }
   return(
     <TouchableWithoutFeedback onPress={ () => Keyboard.dismiss() } >
       <Background>
@@ -31,7 +75,7 @@ export default function RegisterForm(){
 
           <RegisterType type={type} sendTypeChanged={(item) => setType(item)}/>
 
-          <SubmitButton>
+          <SubmitButton onPress={handleSubmit}>
             <SubmitText>Registrar</SubmitText>
           </SubmitButton>
 
